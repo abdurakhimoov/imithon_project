061d9ac8331862273464db12dd49287f5cb53522
Abduqodir <abdurahimovabduqdir96@gmail.com>
2026-01-30 15:56:27 +0500

    first commit

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 from datetime import timedelta
 REST_FRAMEWORK = {
